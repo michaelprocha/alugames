e04ef040e3befab7d65af2de65aa0b3b41a97884
michaelprocha <michaelrocha9722@gmail.com>
2025-08-29 00:03:43 -0300

    add darken image when renting

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 (function () {
 	const games = Array.from(document.getElementsByClassName("dashboard__item__button"));
-
-	games.forEach((game) => {
+	const gamesImage = Array.from(document.getElementsByClassName("dashboard__item__img"));
+	
+	games.forEach((game, i) => {
 		game.addEventListener("click", () => {
 			game.classList.toggle("dashboard__item__button--return");
+			gamesImage[i].classList.toggle("dashboard__item__img--rented");
 			if (game.textContent == "Alugar") {
 				game.textContent = "Devolver";
 			} else {
